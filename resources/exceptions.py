f5efd8af9d3ba05f5ce33f7726bb2dad1f247448
Abdulloh Umar <abdulloh.umar2006@gmail.com>
2024-04-11 15:20:07 +0500

    Update project

--- resources/exceptions.py
+++ resources/exceptions.py
@@ -4,68 +4,59 @@ ERRORS: dict = {}
 
 
 class APIException(HTTPException):
-    status_code: int
 
-    def __init_subclass__(cls):
-        variables = cls.__dict__
-        cls.status_code = variables.get('status_code')
-        ERRORS[cls.__name__] = {'description': cls.__doc__.strip(), 'status_code': cls.status_code}
+    def __init_subclass__(cls, **kwargs):
+        status_code = kwargs.get('status_code')
+        cls.status_code = status_code
+        ERRORS[cls.__name__] = {'description': cls.__doc__.strip(), 'status_code': status_code}
 
     def __init__(self):
         super().__init__(self.status_code, {'status_code': self.status_code, 'error': self.__class__.__name__})
 
 
-class CredentialsException(APIException):
+class CredentialsException(APIException, status_code=status.HTTP_401_UNAUTHORIZED):
     """
     The user's refresh token has expired or the access token is invalid.
     """
-    status_code = status.HTTP_401_UNAUTHORIZED
 
 
-class CaptchaVerifyException(APIException):
+class CaptchaVerifyException(APIException, status_code=status.HTTP_400_BAD_REQUEST):
     """
     The provided captcha could not be verified.
     """
-    status_code = status.HTTP_400_BAD_REQUEST
 
 
-class CaptchaExpiredException(APIException):
+class CaptchaExpiredException(APIException, status_code=status.HTTP_400_BAD_REQUEST):
     """
     The captcha has expired.
     """
-    status_code = status.HTTP_400_BAD_REQUEST
 
 
-class UserAlreadyExistsException(APIException):
+class UserAlreadyExistsException(APIException, status_code=status.HTTP_400_BAD_REQUEST):
     """
     Attempting to create a user that already exists.
     """
-    status_code = status.HTTP_400_BAD_REQUEST
 
 
-class PasswordConfirmException(APIException):
+class PasswordConfirmException(APIException, status_code=status.HTTP_400_BAD_REQUEST):
     """
     The provided password confirmation does not match the original password.
     """
-    status_code = status.HTTP_400_BAD_REQUEST
 
 
-class PasswordValidationException(APIException):
+class PasswordValidationException(APIException, status_code=status.HTTP_400_BAD_REQUEST):
     """
     The provided password does not meet the required criteria.
     """
-    status_code = status.HTTP_400_BAD_REQUEST
 
 
-class UsernameOrPasswordIncorrectException(APIException):
+class UsernameOrPasswordIncorrectException(APIException, status_code=status.HTTP_401_UNAUTHORIZED):
     """
     The username or password provided during authentication is incorrect.
     """
-    status_code = status.HTTP_401_UNAUTHORIZED
 
 
-class OldPasswordIncorrectException(APIException):
+class OldPasswordIncorrectException(APIException, status_code=status.HTTP_401_UNAUTHORIZED):
     """
     The old password provided for a password update operation is incorrect.
     """
-    status_code = status.HTTP_401_UNAUTHORIZED
