598dfe01206faa793318fbd0941977d3daa3d7ea
Abdulloh Umar <abdulloh.umar2006@gmail.com>
2024-04-16 07:38:44 +0500

    Update project

--- routes/users/captcha.py
+++ routes/users/captcha.py
@@ -9,6 +9,6 @@ from urls import user_router
 @user_router.get('/generate-captcha', response_model=APIResponse.example_model(CaptchaModel))
 async def get_new_captcha(request: Request):
     key, img = await generate_captcha(request.app.redis)
-    answer = str(await request.app.redis.get(key))
-    return APIResponse(CaptchaModel(key=key, img=img), answer=answer)
-    # return APIResponse(CaptchaModel(key=key, img=img))
+    # answer = str(await request.app.redis.get(key))
+    # return APIResponse(CaptchaModel(key=key, img=img), answer=answer)
+    return APIResponse(CaptchaModel(key=key, img=img))
