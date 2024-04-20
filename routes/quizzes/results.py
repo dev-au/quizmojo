24ed2bf7a44b108ffa552c7de027122d0d6636f9
Abdulloh Umar <abdulloh.umar2006@gmail.com>
2024-04-20 19:12:51 +0500

    Update project

--- routes/quizzes/results.py
+++ routes/quizzes/results.py
@@ -1,3 +1,7 @@
+import io
+import pandas as pd
+from fastapi import Response
+
 from data.exceptions import *
 from data.models import Quiz, ResultQuiz
 from resources.api_response import APIResponse
@@ -17,4 +21,31 @@ async def result_quizzes(user: CurrentUser, quiz_id: int, page: int):
         results = sorted(results, key=lambda x: (-x.corrects, x.ended_time - x.started_time))
     else:
         results = sorted(results, key=lambda x: (-x.corrects, x.ended_time))
+
     return results[offset: offset + 10]
+
+
+@quiz_router.get('/results-excel', response_class=Response)
+async def result_quizzes_excel(user: CurrentUser, quiz_id: int):
+    quiz = await Quiz.get_or_none(id=quiz_id, owner=user)
+    if not quiz:
+        raise QuizNotFoundException()
+    await ResultQuiz.finish_latecomers(quiz_id)
+    results = await ResultQuiz.filter(quiz=quiz, corrects__ne=-1)
+    data = []
+    for result in results:
+        data.append({
+            "User": result.user.username,
+            "Corrects": result.corrects,
+            "Started Time": result.started_time,
+            "Ended Time": result.ended_time
+        })
+    df = pd.DataFrame(data)
+    excel_file = io.BytesIO()
+    df.to_excel(excel_file, index=False)
+    excel_file.seek(0)
+    return Response(
+        excel_file.getvalue(),
+        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        headers={"Content-Disposition": "attachment;filename=results.xlsx"}
+    )
