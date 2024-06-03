109b83b30ddbcadcc811790931e09ca3c7f79360
Abdulloh Umar <107607543+dev-au@users.noreply.github.com>
2024-06-03 12:54:40 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ REDIS_URL = 'redis://localhost:6379/1'
 
 template = Jinja2Templates(directory='templates')
 
-SECRET_KEY = "1aa7c3c8c5563fb00439b132eb711fe26a36e74b267aa030bbd347fbf2695825"
+SECRET_KEY = "1aa7c3c8c5563fb00439b132eb711fe26a36e74b267a030bbd347fbf269582"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_DAYS = 90
 
